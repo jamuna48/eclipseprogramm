51a107a4b65e4a242a7e5f9d8b793a8d660c0e9b
jamuna48 <jamuna43400@gmail.com>
2024-05-25 14:54:19 +0530

    ok

---
+++ practicedemo/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module practicedemo {
+}
---
+++ practicedemo/src/practicedemo/String34.java
@@ -0,0 +1,11 @@
+package practicedemo;
+
+public class String34 {
+ public static void main (String[]args)
+ {
+	 String name1  = "jamuna";//that is called string literals
+	 name1 = "keerthan";
+	 String name2 = new String("jamuna");// used to creeating object by new key word
+	 System.out.println(name1);
+ } 
+}
