2bff09b1629762a87c7ded67152483b864dda193
jamuna48 <jamuna43400@gmail.com>
2024-05-25 14:53:07 +0530

    ok

---
+++ problemshack/src/problemshack/loop1.java
@@ -0,0 +1,23 @@
+package problemshack;
+import java.util.Scanner;
+public class loop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+		
+		   
+		      Scanner s1 = new Scanner(System.in); 
+		      int N = s1.nextInt();
+		      for(int i =1;i<11;i++)  {
+		          int  answer = N * i;
+		        System.out.println(N + " x " + i +  " = " +  answer); 
+		      } 
+		     
+		     
+		
+
+	}
+
+}
